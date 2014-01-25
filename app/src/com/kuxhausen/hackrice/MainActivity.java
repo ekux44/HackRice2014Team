a8fd5c98dacc554830199d6371986e876d671e87
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-01-25 17:47:37 -0600

    updated to use glass's special camera api
    (background picture taking still not working…)

--- app/src/com/kuxhausen/hackrice/MainActivity.java
+++ app/src/com/kuxhausen/hackrice/MainActivity.java
@@ -1,15 +1,27 @@
 package com.kuxhausen.hackrice;
 
+import java.io.File;
+
 import android.os.Bundle;
+import android.os.FileObserver;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import com.google.android.glass.media.CameraManager;
 
 public class MainActivity extends Activity {
 
+	final static int intentRequestCode = 1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		
+		takePicture();
 	}
 
 	@Override
@@ -18,5 +30,67 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private static final int TAKE_PICTURE_REQUEST = 1;
 
+	private void takePicture() {
+	    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    startActivityForResult(intent, TAKE_PICTURE_REQUEST);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == TAKE_PICTURE_REQUEST && resultCode == RESULT_OK) {
+	        String picturePath = data.getStringExtra(
+	                CameraManager.EXTRA_PICTURE_FILE_PATH);
+	        processPictureWhenReady(picturePath);
+	    }
+
+	    super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	private void processPictureWhenReady(final String picturePath) {
+	    final File pictureFile = new File(picturePath);
+
+	    if (pictureFile.exists()) {
+	        // The picture is ready; process it.
+	    } else {
+	        // The file does not exist yet. Before starting the file observer, you
+	        // can update your UI to let the user know that the application is
+	        // waiting for the picture (for example, by displaying the thumbnail
+	        // image and a progress indicator).
+
+	        final File parentDirectory = pictureFile.getParentFile();
+	        FileObserver observer = new FileObserver(parentDirectory.getPath()) {
+	            // Protect against additional pending events after CLOSE_WRITE is
+	            // handled.
+	            private boolean isFileWritten;
+
+	            @Override
+	            public void onEvent(int event, String path) {
+	                if (!isFileWritten) {
+	                    // For safety, make sure that the file that was created in
+	                    // the directory is actually the one that we're expecting.
+	                    File affectedFile = new File(parentDirectory, path);
+	                    isFileWritten = (event == FileObserver.CLOSE_WRITE
+	                            && affectedFile.equals(pictureFile));
+
+	                    if (isFileWritten) {
+	                        stopWatching();
+
+	                        // Now that the file is ready, recursively call
+	                        // processPictureWhenReady again (on the UI thread).
+	                        runOnUiThread(new Runnable() {
+	                            @Override
+	                            public void run() {
+	                                processPictureWhenReady(picturePath);
+	                            }
+	                        });
+	                    }
+	                }
+	            }
+	        };
+	        observer.startWatching();
+	    }
+	}
 }
