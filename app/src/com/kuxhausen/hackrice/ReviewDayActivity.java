da01a3c9743b233939512127b3c3eb270b0a5914
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-01-25 23:31:28 -0600

    adjusted review my day to full screen image properly

--- app/src/com/kuxhausen/hackrice/ReviewDayActivity.java
+++ app/src/com/kuxhausen/hackrice/ReviewDayActivity.java
@@ -7,6 +7,8 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class ReviewDayActivity extends Activity {
@@ -14,6 +16,11 @@ public class ReviewDayActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		setContentView(R.layout.activity_review);
 		
 		ImageView iOne = (ImageView) this.findViewById(R.id.imageView1);
